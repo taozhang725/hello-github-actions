b45d49b4cbaa9d1ea3b06a926ad435d996249dba
taozhang725 <taozhang725@gmail.com>
2022-05-06 14:40:11 -0700

    Update entrypoint.sh

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/sh -l
 
-sh -c "echo Hello world my name is $INPUT_MY_NAME"
+#sh -c "echo Hello world my name is $INPUT_MY_NAME"
+sh -c "addfile -o CSCwb72397 test1.diff << git diff HEAD^ HEAD"
