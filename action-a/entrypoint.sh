88289ac38fd5d7503f8eb25c7baa63aefc738f3f
taozhang725 <taozhang725@gmail.com>
2022-05-06 15:47:44 -0700

    Update entrypoint.sh

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -2,4 +2,5 @@
 
 #sh -c "echo Hello world my name is $INPUT_MY_NAME"
 echo "new diff" > test.diff
-sh -c "/usr/cisco/bin/addfile -o CSCwb72397 test3.diff test.diff"
+#sh -c "/usr/cisco/bin/addfile -o CSCwb72397 test3.diff test.diff"
+sh -c "ls /usr/cisco/bin/addfile"
